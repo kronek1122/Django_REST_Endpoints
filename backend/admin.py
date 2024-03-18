6349862c280333bbda3a3ab069ba14f277dbf934
kronek <kronkowski.daniel@gmail.com>
2024-03-18 19:54:25 +0100

    register Measurement and HydroponicSystem

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Measurement, HydroponicsSystem
 
-# Register your models here.
+
+admin.site.register(Measurement)
+admin.site.register(HydroponicsSystem)
